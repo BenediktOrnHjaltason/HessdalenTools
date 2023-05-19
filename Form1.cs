e396f95996adce301a55b37754b654629759a12b
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2023-05-19 10:11:15 +0200

    Added acces entity map

--- Form1.cs
+++ Form1.cs
@@ -14,13 +14,19 @@ namespace HessdalenTools
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Dictionary<string,string> accessEntityToLetter = 
+                new Dictionary<string, string>() { { "&AElig;", "Æ" },
+                                                    {"&aelig;", "æ" },
+                                                    {"&Oslash;", "Ø" },
+                                                    {"&oslash;", "ø" },
+                                                    {"&Aring;", "Å" },
+                                                    {"&aring;", "å" }};
+
             var extensions = new List<string>() { "htm", "html" };
 
             var filePathsEN = Directory
                 .EnumerateFiles("C:\\Users\\Admin\\Desktop\\Observations\\EN", "*.*", SearchOption.AllDirectories)
                 .Where(s => extensions.Contains(Path.GetExtension(s).TrimStart('.').ToLowerInvariant()) && !s.Contains("Filer_for"));
-
-
 
             foreach(var path in filePathsEN) { Debug.WriteLine(path); }
 
@@ -29,9 +35,19 @@ namespace HessdalenTools
                 .EnumerateFiles("C:\\Users\\Admin\\Desktop\\Observations\\NO", "*.*", SearchOption.AllDirectories)
                 .Where(s => extensions.Contains(Path.GetExtension(s).TrimStart('.').ToLowerInvariant()) && s.Contains("index"));
 
-            filePathsNO = filePathsNO.Where(x => x.Contains("index"));
-
             foreach (var path in filePathsNO) { Debug.WriteLine(path); }
+
+            for (int i = 1976; i < 2024; i++)
+            {
+                if (filePathsEN.Contains(i.ToString()) && filePathsNO.Contains(i.ToString()))
+                {
+                    StreamReader en = new StreamReader(filePathsEN.Where(x => x.Contains(i.ToString())).FirstOrDefault());
+
+                    StreamReader no = new StreamReader(filePathsNO.Where(x => x.Contains(i.ToString())).FirstOrDefault());
+
+
+                }
+            }
         }
     }
 }
