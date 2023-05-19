da2d60d55c9b5236bb48277ee837f7133b48c0f4
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2023-05-19 09:48:28 +0200

    Getting only the files I need

--- Form1.cs
+++ Form1.cs
@@ -18,15 +18,20 @@ namespace HessdalenTools
 
             var filePathsEN = Directory
                 .EnumerateFiles("C:\\Users\\Admin\\Desktop\\Observations\\EN", "*.*", SearchOption.AllDirectories)
-                .Where(s => extensions.Contains(Path.GetExtension(s).TrimStart('.').ToLowerInvariant()));
+                .Where(s => extensions.Contains(Path.GetExtension(s).TrimStart('.').ToLowerInvariant()) && !s.Contains("Filer_for"));
 
-            Debug.WriteLine($"Found {filePathsEN.Count()} english htm files");
+
+
+            foreach(var path in filePathsEN) { Debug.WriteLine(path); }
+
 
             var filePathsNO = Directory
                 .EnumerateFiles("C:\\Users\\Admin\\Desktop\\Observations\\NO", "*.*", SearchOption.AllDirectories)
-                .Where(s => extensions.Contains(Path.GetExtension(s).TrimStart('.').ToLowerInvariant()));
+                .Where(s => extensions.Contains(Path.GetExtension(s).TrimStart('.').ToLowerInvariant()) && s.Contains("index"));
 
-            Debug.WriteLine($"Found {filePathsNO.Count()} norwegian htm files");
+            filePathsNO = filePathsNO.Where(x => x.Contains("index"));
+
+            foreach (var path in filePathsNO) { Debug.WriteLine(path); }
         }
     }
 }
