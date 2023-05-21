c79965cb756774fc598d0506f895516e6c601298
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2023-05-21 12:20:41 +0200

    finding correct paths with parser

--- Form1.cs
+++ Form1.cs
@@ -39,15 +39,62 @@ namespace HessdalenTools
 
             for (int i = 1976; i < 2024; i++)
             {
-                if (filePathsEN.Contains(i.ToString()) && filePathsNO.Contains(i.ToString()))
+
+                StreamReader srEN = null;
+                StreamReader srNO = null;
+
+                foreach(var path in filePathsEN)
                 {
+                    if (path.Contains(i.ToString()))
+                    {
+                        srEN = new StreamReader(path);
+                    }
+                }
+
+                foreach (var path in filePathsNO) 
+                {  
+                    if (path.Contains(i.ToString())) 
+                    { 
+                        srNO = new StreamReader(path); 
+                    } 
+                }
+
+                if (srEN != null && srNO != null)
+                {
+                    Debug.WriteLine($"Found both paths for {i.ToString()}");
+
                     StreamReader en = new StreamReader(filePathsEN.Where(x => x.Contains(i.ToString())).FirstOrDefault());
 
                     StreamReader no = new StreamReader(filePathsNO.Where(x => x.Contains(i.ToString())).FirstOrDefault());
+
+                    //English
+                    string sampler = "";
+                    while(!en.EndOfStream)
+                    {
+                        sampler = en.ReadLine();
+
+                        if (sampler.Contains("Date"))
+                        {
+                            Debug.WriteLine("Found first line with date");
+                            Debug.WriteLine("stop");
+
+                        }
+                        
+                    }
 
 
                 }
             }
         }
+    }
+
+    public class Report
+    {
+        public string Date;
+        public string Time;
+        public string Description_NO;
+        public string Description_EN;
+        public string ObserverName;
+        public string ObserverPlaceName;
     }
 }
