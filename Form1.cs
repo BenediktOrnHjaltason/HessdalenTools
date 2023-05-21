3efd382d4e3705c4625c1458dd68c5cb625e5a8f
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2023-05-21 22:48:09 +0200

    Matchin reports

--- Form1.cs
+++ Form1.cs
@@ -40,7 +40,8 @@ namespace HessdalenTools
 
             foreach (var path in filePathsNO) { Debug.WriteLine(path); }
 
-            List<Report> reports = new List<Report>();
+            List<Report> norwegianReports = new List<Report>();
+            List<Report> englishReports = new List<Report>();
 
             for (int i = 1976; i < 2024; i++)
             {
@@ -70,96 +71,197 @@ namespace HessdalenTools
                     StreamReader no = new StreamReader(filePathsNO.Where(x => x.Contains(i.ToString())).FirstOrDefault());
 
                     //English
-                    string lineSampler = "";
 
-                    while(!en.EndOfStream)
-                    {
-                        lineSampler = en.ReadLine();
 
-                        if (lineSampler.Contains("Date"))
-                        {
-                            Report report = new Report();
-
-                            int openTags = 0;
-                            int closedTags = 0;
-
-                            for (int j = 0; j < lineSampler.Length; j++)
-                            {
-                                if (lineSampler[j] == '<') 
-                                { 
-                                    if (lineSampler[j + 1] != 'i')
-                                    openTags++; 
-                                }
-                                else if (lineSampler[j] == '>') 
-                                { 
-                                    if (lineSampler[j - 1] != 'i')
-                                    closedTags++; 
-                                }
-
-                                else if (openTags == 2 && closedTags == 2)
-                                {
-                                    report.Date += lineSampler[j];
-                                }
-
-                                else if (openTags == 4 && closedTags == 4)
-                                {
-                                    report.Time += lineSampler[j];
-                                }
-
-                                else if (openTags == 6 && closedTags == 6)
-                                {
-                                    report.ObserverPlaceName += lineSampler[j];
-                                }
-                            }
-
-                            lineSampler = en.ReadLine();
-                            openTags = 0;
-                            closedTags = 0;
-
-                            for (int j = 0; j < lineSampler.Length; j++)
-                            {
-                                if (lineSampler[j] == '<') { openTags++; }
-                                else if (lineSampler[j] == '>') { closedTags++; }
-
-                                if (openTags == 2 && closedTags == 2)
-                                {
-                                    report.ObserverName += lineSampler[j];
-                                }
-                            }
-
-                            //Scrape description:
-
-                            bool doneScrapingDescription = false;
-
-                            while (!doneScrapingDescription)
-                            {
-                                lineSampler = en.ReadLine();
-
-                                if (lineSampler == null || lineSampler.Contains("</p>") || lineSampler.Contains("<p>"))
-                                {
-                                    doneScrapingDescription = true;
-                                }
-                                else
-                                {
-                                    report.Description_EN += lineSampler;
-                                }
-                            }
-
-                            reports.Add(report);
-                        }
-                        
-                    }
+                    ScrapeReports(en, englishReports, "Date");
+                    ScrapeReports(no, norwegianReports, "Dato");
                 }
             }
 
-            foreach (var sampledReport in reports)
+            List<Report> mergedReports = MergeReports(englishReports, norwegianReports);
+
+            foreach (var sampledReport in englishReports)
             {
                 Debug.WriteLine($"Date: {sampledReport.Date}");
                 Debug.WriteLine($"Time: {sampledReport.Time}");
                 Debug.WriteLine($"Place name: {sampledReport.ObserverPlaceName}");
                 Debug.WriteLine($"Observer name: {sampledReport.ObserverName}");
-                Debug.WriteLine($"Description: {sampledReport.Description_EN}");
+                Debug.WriteLine($"Description EN: {sampledReport.Description_EN}");
+                Debug.WriteLine($"Description NO: {sampledReport.Description_NO}");
             }
+        }
+
+        private void ScrapeReports(StreamReader sr, List<Report> reports, string reportStartIdentifier)
+        {
+            string lineSampler = "";
+
+            while (!sr.EndOfStream)
+            {
+                lineSampler = sr.ReadLine();
+
+                if (lineSampler.Contains(reportStartIdentifier))
+                {
+                    Report report = new Report();
+
+                    int openTags = 0;
+                    int closedTags = 0;
+
+                    for (int j = 0; j < lineSampler.Length; j++)
+                    {
+                        if (lineSampler[j] == '<')
+                        {
+                            if (lineSampler[j + 1] != 'i')
+                                openTags++;
+                        }
+                        else if (lineSampler[j] == '>')
+                        {
+                            if (lineSampler[j - 1] != 'i')
+                                closedTags++;
+                        }
+
+                        else if (openTags == 2 && closedTags == 2)
+                        {
+                            report.Date += lineSampler[j];
+                        }
+
+                        else if (openTags == 4 && closedTags == 4)
+                        {
+                            report.Time += lineSampler[j];
+                        }
+
+                        else if (openTags == 6 && closedTags == 6)
+                        {
+                            report.ObserverPlaceName += lineSampler[j];
+                        }
+                    }
+
+                    lineSampler = sr.ReadLine();
+                    openTags = 0;
+                    closedTags = 0;
+
+                    for (int j = 0; j < lineSampler.Length; j++)
+                    {
+                        if (lineSampler[j] == '<') { openTags++; }
+                        else if (lineSampler[j] == '>') { closedTags++; }
+
+                        if (openTags == 2 && closedTags == 2)
+                        {
+                            report.ObserverName += lineSampler[j];
+                        }
+                    }
+
+                    //Scrape description:
+
+                    bool doneScrapingDescription = false;
+
+                    while (!doneScrapingDescription)
+                    {
+                        lineSampler = sr.ReadLine();
+
+                        if (lineSampler == null || lineSampler.Contains("</p>") || lineSampler.Contains("<p>"))
+                        {
+                            doneScrapingDescription = true;
+                        }
+                        else
+                        {
+                            if (reportStartIdentifier == "Date")
+                                report.Description_EN += lineSampler;
+
+                            else if (reportStartIdentifier == "Dato")
+                                report.Description_NO += lineSampler;
+                        }
+                    }
+
+                    reports.Add(report);
+                }
+            }
+
+            CleanReports(reports);
+        }
+
+        private void CleanReports(List<Report> reports)
+        {
+            foreach (Report report in reports) 
+            {
+                if (report.Date != null)
+                {
+                    report.Date.TrimStart();
+                    report.Date.TrimEnd();
+                }
+
+                if (report.Time != null)
+                {
+                    report.Time.TrimStart();
+                    report.Time.TrimEnd();
+                }
+
+                if (report.ObserverName != null)
+                {
+                    report.ObserverName.TrimStart();
+                    report.ObserverName.TrimEnd();
+                }
+
+                if (report.ObserverPlaceName != null)
+                {
+                    report.ObserverPlaceName.TrimStart();
+                    report.ObserverPlaceName.TrimEnd();
+                }
+                
+
+                if (report.Description_NO != null)
+                {
+                    report.Description_NO.TrimStart();
+                    report.Description_NO.TrimEnd();
+                }
+
+                if (report.Description_EN != null)
+                {
+                    report.Description_EN.TrimStart();
+                    report.Description_EN.TrimEnd();
+                }
+            }
+        }
+
+        private List<Report> MergeReports(List<Report> englishReports, List<Report> norwegianReports)
+        {
+            foreach(var report in englishReports) 
+            {
+                Debug.WriteLine($"EN Dates: {report.Date}");
+            }
+            foreach (var report in norwegianReports)
+            {
+                Debug.WriteLine($"NO Observer: {report.Date}");
+            }
+
+
+
+            List<Report> result = new List<Report>();
+
+            Debug.WriteLine($"Norwegian reports: {norwegianReports.Count}. English reports: {englishReports.Count}");
+
+            int mergedReportsCount = 0;
+            for(int i = 0; i < norwegianReports.Count; i++) 
+            {
+                var englishMatches = englishReports.Where(x => x.Date == norwegianReports[i].Date).ToList();
+
+                if (englishMatches.Count() == 1)
+                {
+                    mergedReportsCount++;
+                    result.Add(new Report()
+                    {
+                        Date = norwegianReports[i].Date,
+                        Time = norwegianReports[i].Time,
+                        ObserverName = norwegianReports[i].ObserverName,
+                        ObserverPlaceName = norwegianReports[i].ObserverPlaceName,
+                        Description_EN = englishMatches[0].Description_EN,
+                        Description_NO = norwegianReports[0].Description_NO,
+                    });
+                }
+            }
+
+            Debug.WriteLine($"Merged unequivocal results: {mergedReportsCount}");
+
+            return result;
         }
     }
 
