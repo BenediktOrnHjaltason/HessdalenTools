a4f944aadcbf15fe156d629d06781a0ab0477bb5
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2023-05-21 14:03:02 +0200

    Progress parsing english files

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@ using static System.Net.Mime.MediaTypeNames;
 using System.Windows.Forms;
 using Microsoft.VisualBasic.ApplicationServices;
 using System.Diagnostics;
+using System.Linq;
 
 namespace HessdalenTools
 {
@@ -37,9 +38,10 @@ namespace HessdalenTools
 
             foreach (var path in filePathsNO) { Debug.WriteLine(path); }
 
+            List<Report> reports = new List<Report>();
+
             for (int i = 1976; i < 2024; i++)
             {
-
                 StreamReader srEN = null;
                 StreamReader srNO = null;
 
@@ -68,22 +70,96 @@ namespace HessdalenTools
                     StreamReader no = new StreamReader(filePathsNO.Where(x => x.Contains(i.ToString())).FirstOrDefault());
 
                     //English
-                    string sampler = "";
+                    string lineSampler = "";
+
                     while(!en.EndOfStream)
                     {
-                        sampler = en.ReadLine();
+                        lineSampler = en.ReadLine();
 
-                        if (sampler.Contains("Date"))
+                        if (lineSampler.Contains("Date"))
                         {
-                            Debug.WriteLine("Found first line with date");
-                            Debug.WriteLine("stop");
+                            Report report = new Report();
 
+                            int openTags = 0;
+                            int closedTags = 0;
+
+
+                            for (int j = 0; j < lineSampler.Length; j++)
+                            {
+                                if (lineSampler[j] == '<') 
+                                { 
+                                    if (lineSampler[j + 1] != 'i')
+                                    openTags++; 
+                                }
+                                else if (lineSampler[j] == '>') 
+                                { 
+                                    if (lineSampler[j - 1] != 'i')
+                                    closedTags++; 
+                                }
+
+                                if (openTags == 2 && closedTags == 2)
+                                {
+                                    report.Date += lineSampler[j];
+                                }
+
+                                if (openTags == 4 && closedTags == 4)
+                                {
+                                    report.Time += lineSampler[j];
+                                }
+
+                                if (openTags == 6 && closedTags == 6)
+                                {
+                                    report.ObserverPlaceName += lineSampler[j];
+                                }
+                            }
+
+                            lineSampler = en.ReadLine();
+                            openTags = 0;
+                            closedTags = 0;
+
+                            for (int j = 0; j < lineSampler.Length; j++)
+                            {
+                                if (lineSampler[j] == '<') { openTags++; }
+                                else if (lineSampler[j] == '>') { closedTags++; }
+
+                                if (openTags == 2 && closedTags == 2)
+                                {
+                                    report.ObserverName += lineSampler[j];
+                                }
+                            }
+
+                            //Scrape description:
+
+                            bool doneScrapingDescription = false;
+
+                            while (!doneScrapingDescription)
+                            {
+                                lineSampler = en.ReadLine();
+
+                                if (lineSampler == null || lineSampler.Contains("</p>") || lineSampler.Contains("<p>"))
+                                {
+                                    doneScrapingDescription = true;
+                                }
+                                else
+                                {
+                                    report.Description_EN += lineSampler;
+                                }
+                            }
+
+                            reports.Add(report);
                         }
                         
                     }
-
-
                 }
+            }
+
+            foreach (var sampledReport in reports)
+            {
+                Debug.WriteLine($"Date: {sampledReport.Date}");
+                Debug.WriteLine($"Time: {sampledReport.Time}");
+                Debug.WriteLine($"Place name: {sampledReport.ObserverPlaceName}");
+                Debug.WriteLine($"Observer name: {sampledReport.ObserverName}");
+                Debug.WriteLine($"Description: {sampledReport.Description_EN}");
             }
         }
     }
